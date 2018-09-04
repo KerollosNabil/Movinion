1f2e2b0572c2a4a08e6a6d3b42b91ef44412f255
kerollos nabil <kerollosnabil@kerolloss-Mac.local>
2018-09-04 05:03:00 -0700

    Initial Commit

---
+++ Movies App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Movies App
+//
+//  Created by kerollos nabil on 9/4/18.
+//  Copyright © 2018 kerollos nabil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Movies App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Movies App
+//
+//  Created by kerollos nabil on 9/4/18.
+//  Copyright © 2018 kerollos nabil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
